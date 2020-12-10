50f490d1f7e6997371c23fae584d1b8b7fc51fbb
Plaing7808 <61950009+Plaing7808@users.noreply.github.com>
2020-12-10 10:32:41 -0800

    Main

--- main-frame-todo-frontend-master/src/app/tasks-data.service.ts
+++ main-frame-todo-frontend-master/src/app/tasks-data.service.ts
@@ -10,26 +10,36 @@ import { WebrequestService } from './webrequest.service';
 export class TasksDataService {
 
     constructor(private webRequestService: WebrequestService) { }
+    getUsers() {
+        return this.webRequestService.get('user');
+    }
 
     createUser(title: string) {
         return this.webRequestService.post('user', { title });
     }
-    deleteUser(id: string){
+    
+    deleteUser(id: string) {
         return this.webRequestService.delete(`user/${id}`);
     }
 
-    updateUser(id:string, title: string){
-        return this.webRequestService.patch(`user/${id}`,{title});
-    }
-
-    getUsers() {
-        return this.webRequestService.get('user');
+    updateUser(id: string, title: string) {
+        return this.webRequestService.patch(`user/${id}`, { title });
     }
 
     getTasks(userId: string) {
         return this.webRequestService.get(`user/${userId}/task`);
     }
-    createTask(title: string, userId: string){
-        return this.webRequestService.post(`user/${userId}/task`,{title})
-            }
+
+    createTask(title: string, userId: string) {
+        return this.webRequestService.post(`user/${userId}/task`, { title });
+    }
+
+    deleteTask(userId: string, taskId: string) {
+        return this.webRequestService.delete(`user/${userId}/task/${taskId}`);
+    }
+
+    updateTask(userId: string, taskId: string, title: string) {
+        return this.webRequestService.patch(`user/${userId}/task/${taskId}`, { title })
+    }
+
 }
